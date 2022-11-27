16b50a4724f2afd8b77fa788d8781baa24ca517a
MysticalMist123 <70788854+MysticalMist123@users.noreply.github.com>
2022-11-27 10:22:25 +0530

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,84 @@
+//--------------------------------------------IMPORTANT: try not to change this-----------------------------------------------
+// console.log("yo")
+// let getAll_ep = 'http://localhost:3000/getAll'
+let insert_ep = 'http://localhost:3000/insert'
+let del_ep = 'http://localhost:3000/delete'
+let upd_ep = 'http://localhost:3000/update'
+
+function current_date(){
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+  
+    return yyyy +'-'+ mm + '-' + dd;
+}
+
+// async function getAll(){
+//     console.log("getAll button clicked")
+//     fetch(getAll_ep).then((res)=>res.json()).then((data)=>(
+//         console.log(data)
+//     ))
+// }
+
+async function insert(table_name,insert_values){
+    console.log('insert button clicked')
+    fetch(insert_ep,{
+        method: 'POST',
+        body: JSON.stringify({
+            table_name : table_name,
+            insert_values : insert_values
+        }),
+        headers: {
+            'Content-type':'application/json; charset=UTF-8'
+        }
+    }).then((data)=>(
+        console.log(`inserted record.`)
+    ))
+}
+
+async function del(table_name,del_keys){
+    console.log('delete button clicked')
+    fetch(del_ep,{
+        method:'POST',
+        body: JSON.stringify({
+            table_name : table_name,
+            del_keys : del_keys
+        }),
+        headers: {
+            'Content-type':'application/json; charset=UTF-8'
+        }
+    }).then((data)=>(
+        console.log(`deleted record `)
+    ))
+}
+
+async function update(table_name,col,new_val,identifiers){
+    console.log('update button clicked')
+    fetch(upd_ep,{
+        method:'POST',
+        body: JSON.stringify({
+            table_name : table_name,
+            col: col,
+            new_val: new_val,
+            identifiers : identifiers
+        }),
+        headers: {
+            'Content-type':'application/json; charset=UTF-8'
+        }
+    }).then((data)=>(
+        console.log(`updated record`)
+    ))
+}
+
+//+++++++++++++++++++++++++++++++++++++++ HTML JS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+/*
+to insert, call insert(table_name,[value1,value2,value3,.....,valueN])
+
+to delete, call del(table_name,[[ col_name1,cell_value1 ],[ col_name2,cell_value2 ],.......])
+(basically, delete from {table_name} where {col_name1} = {cell_value1} and {col_name2} = {cell_value2} and......)
+
+to update, call update(table_name,column_name,new_value,[[ col_name1,cell_value1 ],[ col_name2,cell_value2 ],.......])
+(basically, update {table_name} set {column_name} = {new_val} where {col_name1} = {cell_value1} and {col_name2} = {cell_value2} and......)
+*/
---
+++ server.js
@@ -0,0 +1,136 @@
+//------------------------------------------------INITIAL CONFIGS-------------------------------------------------------------
+const express = require('express')
+const sql = require('mysql')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+// NOTE: use your sql credentials here
+// NOTE: turn the server on, using "node server.js" on your terminal
+let con = sql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Stkl@1210"
+})
+
+let use = 'use dbms_project;'
+
+//--------------------------------------------------EVENT AND REQUEST HANDLING-------------------------------------------------
+app.use(cors({origin:'*'}))
+app.use(express.json())
+
+// app.get('/getAll',(req,res)=>{
+//   showAll().then((result)=>{res.send(result)})
+// })
+app.post('/insert',(req,res)=>{
+  insert(req.body['table_name'],req.body['insert_values'])
+  res.send('DONE insertion!')
+})
+app.post('/delete',(req,res)=>{
+  del(req.body['table_name'],req.body['del_keys'])
+  res.send('DONE deletion!')
+})
+app.post('/update',(req,res)=>{
+  update(req.body['table_name'],req.body['col'],req.body['new_val'],req.body['identifiers'])
+  res.send('DONE updating')
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+//---------------------------------------------------SQL commands and associate functions--------------------------------------
+
+function current_date(){
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, '0');
+  let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  let yyyy = today.getFullYear();
+
+  return yyyy +'-'+ mm + '-' + dd;
+}
+
+function insert(table_name,insert_values){
+  let s=""
+  for(let i=0;i<insert_values.length;i++){
+    if(Number.isInteger(insert_values[i])){
+      s+=`${insert_values[i]},`
+    }
+    else{
+      s+=`"${insert_values[i]}",`
+    }
+  }
+  s=s.slice(0,-1)
+
+  let query = `insert into ${table_name} values(${s})`
+  con.query(use,(err,result)=>{
+    if(err) throw err
+    console.log("using database....")
+    con.query(query,(err,result)=>{
+      if(err) throw err
+      console.log("inserted successfully!")
+    })
+  })
+}
+
+function del(table_name,del_keys = []){ //del_keys is an array of arrays. ( [[col_name,col_val],[],[]....] )
+  let query = ""
+  if(del_keys.length == 0)
+    query = `delete from ${table_name}`
+  else{
+    if(typeof(del_keys[0][1])!='number') del_keys[0][1] = `'${del_keys[0][1]}'`
+    query = `delete from ${table_name} where ${del_keys[0][0]} = ${del_keys[0][1]}`
+    for(let i=1;i<del_keys.length;i++){
+      if(typeof(del_keys[i][1])!='number') del_keys[i][1] = `'${del_keys[i][1]}'`
+      query+=`and ${del_keys[i][0]} = "${del_keys[i][1]}"`
+    }
+  }
+  con.query(use,(err,result)=>{
+    if(err) throw err
+    console.log("using database....")
+    con.query(query,(err,result)=>{
+      if(err) throw err
+      console.log(`deleted successfully!`)
+    })
+  })
+}
+
+function update(table_name,col,new_val,identifiers){ //identifiers work as del_keys of del function
+  query = ""
+  if(typeof(new_val)!='number') new_val = `'${new_val}'`
+
+  if(typeof(identifiers[0][1])!='number') identifiers[0][1] = `'${identifiers[0][1]}'`
+  query = `update ${table_name} set ${col} = ${new_val} where ${identifiers[0][0]} = ${identifiers[0][1]}`
+  for(let i=1;i<identifiers.length;i++){
+    if(typeof(identifiers[i][1])!='number') identifiers[i][1] = `'${identifiers[i][1]}'`
+    query+=` and ${identifiers[i][0]} = ${identifiers[i][1]}`
+  }
+
+  con.query(use,(err,result)=>{
+    if(err) throw err
+    console.log("using database....")
+    con.query(query,(err,result)=>{
+      if(err) throw err
+      console.log(`updated`)
+    })
+  })
+}
+
+// function showAll(){ //returns promise
+//   return new Promise(function(resolve,reject){
+//     query = `select * from user`
+//     con.query(use,(err,result)=>{
+//       if(err) throw err
+//       console.log("using database....")
+//       con.query(query,(err,result,fields)=>{
+//         if(err) reject(new Error(err))
+//         else resolve(result)
+//         console.log("selected all and sent.")
+//       })
+//     })
+//   })
+// }
+
+// insert("course_details",["dbms",48,20,30,20,30,50,50,10,10,10,10,"1201"])
+// del("stu_course",[["course_id","dbms"]])
+// update("stu_course","ct1_score",18,[["course_id","dbms"]])
+// showAll().then((res)=>{console.log(res)})
