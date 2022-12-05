3587879a7b963295d8de66d839cd525865476c64
kiran <abdalhgn@gmail.com>
2022-12-05 12:55:25 +0530

    add records to user too

--- dummy_records.py
+++ dummy_records.py
@@ -21,18 +21,19 @@ for course_id in range(1,6):
     record = f"'{str(course_id)}', {numClasses}, "
     for i in range(5):
         record += f"{totalMarkList[i]}, {totalWeightList[i]}, "
-    record += f"'{str(course_id)}'"
+    record += f"'{str(course_id+5)}'"
     cursor.execute(f'insert into course_details values (' + record + f');')
     db.commit()
 
 
-# populate stu_course
+# populate stu_course and user
 for stu in range(1,6):
+    cursor.execute(f'insert into user values ("{stu}", "123", "John Doe {stu}", "student")')
+    cursor.execute(f'insert into user values ("{stu+5}", "123", "John Doe {stu+5}", "professor")')
     for course in range(1,6):
         cursor.execute(f'select * from course_details where course_id = "{course}";')
         record = cursor.fetchall()[0]
         newRecord = f"'{stu}', '{course}', {randint(0,record[1])}"
-        print(record)
         for i in range(2, 11, 2):
             newRecord += f", {randint(0,record[i])}"
         cursor.execute(f'insert into stu_course values (' + newRecord + f');')
