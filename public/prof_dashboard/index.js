e646f762fb7afe5e8fcaff8bf9752fe7135c9d4a
tarun <tarun.k.lywait@gmail.com>
2022-12-05 10:01:38 +0530

    save_button update done

--- public/prof_dashboard/index.js
+++ public/prof_dashboard/index.js
@@ -52,12 +52,17 @@ save_button.addEventListener('click',(e)=>{
     let endsem_score = document.getElementById('endsem_score').value
     let pre_days = document.getElementById('pre_days').value
 
-    s.exec(`update stu_course set ct1_score = ${ct1_score} where roll_no = '${sroll_no.value}' and course_id = '${cid.value}'`)
-    s.exec(`update stu_course set ct2_score = ${ct2_score} where roll_no = '${sroll_no.value}' and course_id = '${cid.value}'`)
-    s.exec(`update stu_course set assign1_score = ${ass1_score} where roll_no = '${sroll_no.value}' and course_id = '${cid.value}'`)
-    s.exec(`update stu_course set assign2_score = ${ass2_score} where roll_no = '${sroll_no.value}' and course_id = '${cid.value}'`)
-    s.exec(`update stu_course set endsem_score = ${endsem_score} where roll_no = '${sroll_no.value}' and course_id = '${cid.value}'`)
-    s.exec(`update stu_course set no_of_presents = ${pre_days} where roll_no = '${sroll_no.value}' and course_id = '${cid.value}'`)
+    s.exec(`update stu_course set ct1_score = ${ct1_score} where roll_no = '${sroll_no.value}' and course_id = '${cid.value}'`).then(()=>{
+        s.exec(`update stu_course set ct2_score = ${ct2_score} where roll_no = '${sroll_no.value}' and course_id = '${cid.value}'`).then(()=>{
+            s.exec(`update stu_course set assign1_score = ${ass1_score} where roll_no = '${sroll_no.value}' and course_id = '${cid.value}'`).then(()=>{
+                s.exec(`update stu_course set assign2_score = ${ass2_score} where roll_no = '${sroll_no.value}' and course_id = '${cid.value}'`).then(()=>{
+                    s.exec(`update stu_course set endsem_score = ${endsem_score} where roll_no = '${sroll_no.value}' and course_id = '${cid.value}'`).then(()=>{
+                        s.exec(`update stu_course set no_of_presents = ${pre_days} where roll_no = '${sroll_no.value}' and course_id = '${cid.value}'`)
+                    })
+                })
+            })
+        })
+    })
 
     alert('Changes saved!')
 })
